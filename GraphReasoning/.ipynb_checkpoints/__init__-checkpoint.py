9ddedbc7a75308421a692f961c8aa157d14575f9
GiWook Hong <rldnr7959@DESKTOP-P7JI0EN.localdomain>
2026-08-13 10:44:30 +0900

    chore: stop tracking generated and temporary files

--- GraphReasoning/.ipynb_checkpoints/__init__-checkpoint.py
+++
@@ -1,6 +0,0 @@
-from GraphReasoning.openai_tools import *
-from GraphReasoning.graph_tools import *
-from GraphReasoning.graph_generation import *
-from GraphReasoning.utils import *
-from GraphReasoning.graph_analysis import *
-from GraphReasoning.agents import *
--- Notebooks/.ipynb_checkpoints/rsync-checkpoint.sh
+++
@@ -1,3 +0,0 @@
-# cp -r ../* ../../GraphReasoning_SG/ 
-rsync -av --no-links --progress ~/workspace/LLM/GraphReasoning_working/Notebooks/SG/ ~/workspace/LLM/GraphReasoning_SG/Notebooks/SG --exclude temp --exclude TSMC --exclude chroma --exclude cache --exclude ipynb_checkpoints
-  
